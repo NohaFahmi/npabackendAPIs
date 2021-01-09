c745c9c335363f2eb26b2961ffbbb768e2df84df
Noha M <nm1488@fayoum.edu.eg>
2021-01-09 14:43:46 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const morgan = require('morgan')
+var cors = require('cors')
+const app = express();
+const path = require('path')
+
+//setup logger
+app.use(morgan('dev'))
+
+//using cors
+app.use(cors())
+
+//connecting with DB
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+app.use(express.json());
+db.once('open', () => console.log('connected to database'));
+//creating routes
+const teamsRouter = require('./routes/teams');
+const homeRouter = require('./routes/home');
+const subscriptionRouter = require('./routes/subscriptions');
+const logoRouter = require('./routes/logos');
+
+app.use('/api/teams', teamsRouter);
+app.use('/api/home', homeRouter);
+app.use('/api/subscriptions', subscriptionRouter);
+app.use('/api/logos', logoRouter);
+
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, 'index.html')));
+
+// app.use(express.static('npateams/build'));
+app.use('/*', (req, res) => {
+    res.status(404).send('<h1>NOT FOUND</h1>')
+})
+
+//server listen on port
+const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || config.httpPort;
+
+app.listen(PORT, () => console.log(`Your server is running on: http://localhost:${PORT}/`));
